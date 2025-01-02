de17ec386506ddc14cfdc7a50e636c744cdb9293
vishal thakur <thecodevishal@gmail.com>
2025-01-03 00:34:25 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./Store.jsx";
+import { store } from "./store.jsx";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
